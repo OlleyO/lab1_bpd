3b94f41e82cabc2630906ec1decc0334632cd542
Oleh <my.mail.olech@gmail.com>
2022-08-17 23:30:43 +0300

    feat: checksum using md5 algorithm

--- lib/screens/lab2_screen.dart
+++ lib/screens/lab2_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:lab_1/constants/app_styles.dart';
 
@@ -20,6 +23,11 @@ class Lab2Screen extends StatefulWidget {
 // print(result.stdout);
 
 class _Lab2ScreenState extends State<Lab2Screen> {
+  final toHashController = TextEditingController();
+  final hashedController = TextEditingController();
+  final checksumController1 = TextEditingController();
+  final checksumController2 = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,7 +36,7 @@ class _Lab2ScreenState extends State<Lab2Screen> {
       ),
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.all(15.0),
+          padding: const EdgeInsets.all(15.0),
           child: Row(
             children: <Widget>[
               Expanded(
@@ -41,77 +49,175 @@ class _Lab2ScreenState extends State<Lab2Screen> {
                         ListTile(
                           contentPadding: EdgeInsets.zero,
                           title: TextField(
+                            controller: toHashController,
                             decoration: InputDecoration(
                               labelText: "Текст",
                             ),
                           ),
                           trailing: IconButton(
-                            icon: Icon(Icons.file_copy),
-                            onPressed: () {},
+                            icon: Icon(Icons.folder_open_outlined),
+                            onPressed: () async {
+                              final result =
+                                  await FilePicker.platform.pickFiles(
+                                dialogTitle: "Оберіть файл для зчитування",
+                                lockParentWindow: true,
+                              );
+
+                              if (result == null) {
+                              } else {
+                                final file = File(result.files.first.path!);
+
+                                final text = await file.readAsString();
+
+                                print(text);
+
+                                toHashController.value = TextEditingValue(
+                                  text: text,
+                                  selection: TextSelection.fromPosition(
+                                    TextPosition(offset: text.length),
+                                  ),
+                                );
+                              }
+                            },
                           ),
                         ),
-                        SizedBox(
+                        const SizedBox(
                           height: 15.0,
                         ),
                         TextField(
+                          controller: hashedController,
                           readOnly: true,
                           decoration: InputDecoration(
                             labelText: "Захешований текст",
                           ),
                         ),
-                        SizedBox(
+                        const SizedBox(
                           height: 15.0,
                         ),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
                             ElevatedButton(
-                              onPressed: () {},
-                              child: Text(
+                              onPressed: () async {
+                                var result = await Process.run(
+                                  "D:\\labs_bpd\\lab2_c\\x64\\Release\\lab2_c",
+                                  [
+                                    "-i",
+                                    toHashController.text,
+                                  ],
+                                );
+
+                                hashedController.value = TextEditingValue(
+                                    text: result.stdout.toString());
+
+                                // setState(() {
+                                //   hashedText = result.stdout.toString();
+                                // });
+                              },
+                              child: const Text(
                                 "Захешувати",
                               ),
                             ),
                             ElevatedButton(
-                              onPressed: () {},
+                              onPressed: () async {
+                                String? outputFile =
+                                    await FilePicker.platform.saveFile(
+                                  dialogTitle: "Оберіть файл:",
+                                  fileName: "lab2_output.txt",
+                                );
+
+                                if (outputFile == null) {
+                                } else {
+                                  var file = await File(outputFile)
+                                      .writeAsString(hashedController.text);
+                                }
+                              },
                               child: Text(
-                                "Зберегти до файлу",
+                                "Зберегти",
                               ),
                             ),
                           ],
                         ),
-                        SizedBox(
+                        const SizedBox(
                           height: 50.0,
                         ),
-                        Divider(),
-                        SizedBox(
+                        const Divider(),
+                        const SizedBox(
                           height: 50.0,
                         ),
                         Text(
                           "Перевірка цілісності з використанням контрольної суми",
                           style: AppStyles.titleText,
                         ),
-                        SizedBox(
+                        const SizedBox(
                           height: 25.0,
                         ),
                         ListTile(
                           contentPadding: EdgeInsets.zero,
                           title: TextField(
+                            readOnly: true,
+                            controller: checksumController1,
                             decoration: InputDecoration(
-                              labelText: "Текст",
+                              labelText: "Файл1",
                             ),
                           ),
                           trailing: IconButton(
-                            icon: Icon(Icons.file_copy),
-                            onPressed: () {},
+                            icon: Icon(Icons.folder_open_outlined),
+                            onPressed: () async {
+                              final res = await FilePicker.platform
+                                  .pickFiles(dialogTitle: "Оберіть файл:");
+
+                              if (res != null) {
+                                checksumController1.value =
+                                    TextEditingValue(text: res.paths.first!);
+                              }
+                            },
                           ),
                         ),
-                        SizedBox(
+                        const SizedBox(
+                          height: 15.0,
+                        ),
+                        ListTile(
+                          contentPadding: EdgeInsets.zero,
+                          title: TextField(
+                            readOnly: true,
+                            controller: checksumController2,
+                            decoration: InputDecoration(
+                              labelText: "Файл2",
+                            ),
+                          ),
+                          trailing: IconButton(
+                            icon: Icon(Icons.folder_open_outlined),
+                            onPressed: () async {
+                              final res = await FilePicker.platform
+                                  .pickFiles(dialogTitle: "Оберіть файл:");
+
+                              if (res != null) {
+                                checksumController2.value =
+                                    TextEditingValue(text: res.paths.first!);
+                              }
+                            },
+                          ),
+                        ),
+                        const SizedBox(
                           height: 15.0,
                         ),
                         Align(
                           alignment: Alignment.centerRight,
                           child: ElevatedButton(
-                            onPressed: () {},
+                            onPressed: () async {
+                              var result = await Process.run(
+                                "D:\\labs_bpd\\lab2_c\\x64\\Release\\lab2_c",
+                                [
+                                  "-if",
+                                  checksumController1.text,
+                                  "-cif",
+                                  checksumController2.text,
+                                ],
+                              );
+
+                              print(result.stdout);
+                            },
                             child: Text(
                               "Перевірити",
                             ),
