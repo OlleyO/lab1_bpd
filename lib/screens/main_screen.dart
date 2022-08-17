068a9d43bb1b86aa0fb1889155f32f7404a48641
Oleh <my.mail.olech@gmail.com>
2022-08-17 12:52:13 +0300

    feat: main screen with links for navigation

---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:lab_1/constants/app_styles.dart';
+import 'package:lab_1/screens/lab1_screen.dart';
+
+class MainScreen extends StatelessWidget {
+  static const String path = "home";
+
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: ListTile(
+                onTap: () {
+                  Navigator.of(context).pushNamed(Lab1Screen.path);
+                },
+                title: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Лабораторна 1",
+                    style: AppStyles.titleText,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: ListTile(
+                onTap: () {},
+                title: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Лабораторна 2",
+                    style: AppStyles.titleText,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: ListTile(
+                onTap: () {},
+                title: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Лабораторна 3",
+                    style: AppStyles.titleText,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: ListTile(
+                onTap: () {},
+                title: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Лабораторна 4",
+                    style: AppStyles.titleText,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: ListTile(
+                onTap: () {},
+                title: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Лабораторна 5",
+                    style: AppStyles.titleText,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
